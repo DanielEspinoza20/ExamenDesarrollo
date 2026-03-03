6bb9d57f6cc3407e2c1e1202b7b40b4d41cb524e
DanielEspinoza20 <daniel.espinoza.cano@uabc.edu.mx>
2026-03-02 19:52:46 -0800

    Se definio los atributos y validación como tambien no aceptar campos vacios en el inicio de sesión

--- persistencia/src/main/java/mx/SauapELS/dao/UsuarioDAO.java
+++ persistencia/src/main/java/mx/SauapELS/dao/UsuarioDAO.java
@@ -20,6 +20,19 @@ public class UsuarioDAO extends AbstractDAO<Usuario> {
                 .getResultList();
     }
 
+    public Usuario login(String correo, String contrasena) {
+            List<Usuario> lista = entityManager.createQuery(
+                            "SELECT u FROM Usuario u WHERE u.correo = :correo AND u.contrasena = :contrasena", Usuario.class)
+                    .setParameter("correo", correo)
+                    .setParameter("contrasena", contrasena)
+                    .getResultList();
+            if (lista.isEmpty()) {
+                return null;
+            }
+            return lista.get(0);
+        }
+
+
     @Override
     public EntityManager getEntityManager() {
         return entityManager;
--- vista/src/main/java/ui/LoginBeanUI.java
+++ vista/src/main/java/ui/LoginBeanUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ui;
 
 import helper.LoginHelper;
@@ -18,41 +13,59 @@ import java.io.Serializable;
 
 @Named("loginUI")
 @SessionScoped
-public class LoginBeanUI implements Serializable{
+public class LoginBeanUI implements Serializable {
+
     private LoginHelper loginHelper;
     private Usuario usuario;
-    
+
     public LoginBeanUI() {
         loginHelper = new LoginHelper();
     }
-    
-    /**
-     * Metodo postconstructor todo lo que este dentro de este metodo
-     * sera la primero que haga cuando cargue la pagina
-     */
+
     @PostConstruct
-    public void init(){
-        usuario= new Usuario();
+    public void init() {
+        usuario = new Usuario();
     }
 
-     public void login() throws IOException{
-        String appURL = "/index.xhtml";
-        // los atributos de usuario vienen del xhtml 
-        Usuario us= new Usuario();
-        us.setId(0);
-        us = loginHelper.Login(usuario.getCorreo(), usuario.getContrasena());
-          if(us != null && us.getId()!=null){
-            // asigno el usuario encontrado al usuario de esta clase para que 
-            // se muestre correctamente en la pagina de informacion
-            usuario=us;
-            FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + appURL);
-        }else{
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Usuario o contraseña incorrecta:", "Intente de nuevo"));
+    public void login() throws IOException {
+
+        if (usuario.getCorreo() == null || usuario.getCorreo().trim().isEmpty()
+                || usuario.getContrasena() == null || usuario.getContrasena().trim().isEmpty()) {
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Error",
+                            "Usuario y contraseña son obligatorios"));
+
+            return;
+        }
+
+        Usuario us = loginHelper.Login(usuario.getCorreo(), usuario.getContrasena());
+
+        if (us == null) {
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Error",
+                            "Usuario o contraseña incorrectos"));
+
+        } else {
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO,
+                            "Correcto",
+                            "Acceso permitido"));
+
+            usuario = us;
+
+            FacesContext.getCurrentInstance()
+                    .getExternalContext()
+                    .redirect(FacesContext.getCurrentInstance()
+                            .getExternalContext()
+                            .getRequestContextPath() + "/index.xhtml");
         }
     }
 
-    
-    /* getters y setters*/
 
     public Usuario getUsuario() {
         return usuario;
@@ -61,16 +74,4 @@ public class LoginBeanUI implements Serializable{
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-
-    
 }
